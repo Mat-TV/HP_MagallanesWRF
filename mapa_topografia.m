c76479658d1cde61e4d4a4e4b2f45ebc11d5febb
Mat Troncoso Villar <82126178+Trufumut@users.noreply.github.com>
2022-07-03 01:26:10 -0400

    Add files via upload
    Agregada la línea de costa de alta resolución de gshhs

--- mapa_topografia.m
+++ mapa_topografia.m
@@ -19,23 +19,22 @@ hgt=ncread([PATHin,filename],'HGT',[1,1,1],[100,100,1]);
 %%
 figure
 m_proj('lambert','lon',[min(min(double(lon))) max(max(double(lon)))],...
-'lat',[min(min(double(lat))) max(max(double(lat)))]);
+        'lat',[min(min(double(lat))) max(max(double(lat)))]);
 m_pcolor(lon(:,:),lat(:,:),hgt(:,:));
 m_grid('box','fancy','tickdir','in');
-
-hc=colormap(flipud(gray));
-title(hc,'[m]');
-
+colormap(flipud(gray));
 ax1=m_contfbar(.97,[.5 .9],hgt(:,:), ...
     [64],'edgecolor','none','endpiece','no');
 title(ax1,{'Altitud [m]',''}); % Move up by inserting a blank line
 % colormap([m_colmap('blues',80);m_colmap('gland',48)]);
-title('Topografía del dominio 3')
+% m_gshhs_h('color','k'); % por si es que no se guarda gumby antes
+m_usercoast('gumby','linewidth',1.2,'color','k'); %alternativa a m_gshhs_h
+title('Topografía del dominio 3.')
 xlabel('Longitud','fontsize',12)
 ylabel('Latitud','fontsize',12)
 %   x_width=17.12 ;y_width=10.34;
 %   cd 'C:\Users\fredo\OneDrive\Documents\Geof\Modelación numérica de la atmósfera\Trabajo 1\Gráficos';
 %   set(gcf, 'PaperPosition', [0 0 x_width y_width]);
-  saveas(gcf,'mapa_topografia.png')
+  saveas(gcf,[PATHout,'mapa_topografia'],'png')
 %   cd ..
 end
